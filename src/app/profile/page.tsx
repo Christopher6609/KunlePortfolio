09445673c9184c7faa77d49bc393e2406abd8d8a
Christopher <odeniyichristopher7@gmail.com>
2024-10-28 12:15:19 +0000

    client feedback section

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,4 @@
+import Feedback from "../components/Feedback/Feedback";
 import Heading from "../components/Heading/Heading";
 import Linkers from "../components/Linkers/Linkers";
 import ProfileInfo from "../components/ProfileInfo/ProfileInfo";
@@ -9,7 +10,9 @@ const Profile = () => {
             <Heading headingLineOne="Hi, I'm" headingLineTwo="Adekunle Oladosu" headingDescription="Easily track, analyze, and optimize your investments in real-time. Our intuitive portfolio management tools provide personalized insights, helping you make data-driven decisions to grow your assets." mouseText="Read My Story" scrollToLocation="#story" />
             <ProfileInfo />
             <Services />
+            <Feedback />
             <Linkers />
+           
 
 
         </div>
