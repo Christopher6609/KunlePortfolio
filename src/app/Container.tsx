a301c2eb7e6e3edfe00d648521a5d7fca68ce956
Christopher <odeniyichristopher7@gmail.com>
2024-11-12 01:08:45 +0000

    project page responsiveness

--- src/app/Container.tsx
+++ src/app/Container.tsx
@@ -6,7 +6,7 @@ type containerProps = {
 
 const Container = ({ children }: containerProps) => {
     return (
-        <div className="max-w-[1440px] mx-auto px-[5.81rem]">
+        <div className="max-w-[1440px] mx-auto md:px-[5.81rem] px-[1rem]">
             {children}
         </div>
     )
--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -6,10 +6,19 @@ const page = () => {
         <div className="mt-[6rem]">
             <div className="h-[43.8125rem]"></div>
             <div className='md:flex py-[5rem] items-center justify-between'>
-                <div className='space-y-[1.5rem] w-[28rem] '>
-                    <button className={`border text-[0.875rem] border-[#DAC5A7] py-[0.25rem] px-[0.5rem] bg-[#DAC5A7] rounded text-[#000] hover:text-[#000]`}>Web 3</button>
-                    <h1 className='text-[3.5rem] leading-[4.2rem] font-[500]'>Begreat Finance Management</h1>
-                    <p className='text-[1rem] leading-[1.5rem] font-[200]'>Web-design, Mobile App, Dashboard design</p>
+                <div className='space-y-[1.5rem] md:w-[28rem] '>
+                    <button className={`border text-[0.875rem] border-[#DAC5A7] py-[0.25rem] px-[0.5rem] bg-[#DAC5A7] rounded text-[#000] hover:text-[#000] hidden md:block`}>Web 3</button>
+                    <h1 className='md:text-[3.5rem] text-[2.5rem] md:text-start text-center md:leading-[4.2rem] leading-[3rem] font-[500]'>Begreat Finance Management</h1>
+                    <p className='text-[1rem] leading-[1.5rem] font-[200] hidden md:block'>Web-design, Mobile App, Dashboard design</p>
+                    <div className=' md:hidden space-y-[1.5rem] pb-[2rem]'>
+                        <p className='text-[1rem] leading-[1.5rem]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+                        <div className='flex gap-4'>
+                            <button className={`border text-[0.875rem] border-[#DAC5A7] py-[0.25rem] px-[0.5rem] bg-[#DAC5A7] rounded text-[#000] hover:text-[#000]`}>Tag one</button>
+                            <button className={`border text-[0.875rem] border-[#DAC5A7] py-[0.25rem] px-[0.5rem] bg-[#DAC5A7] rounded text-[#000] hover:text-[#000]`}>Tag two</button>
+                            <button className={`border text-[0.875rem] border-[#DAC5A7] py-[0.25rem] px-[0.5rem] bg-[#DAC5A7] rounded text-[#000] hover:text-[#000]`}>Tag three</button>
+                        </div>
+                    </div>
+
                 </div>
                 <div className='space-y-[2rem]'>
                     <div className='flex justify-between'>
@@ -34,9 +43,9 @@ const page = () => {
                     </div>
                 </div>
             </div>
-            <div className='py-[7rem] flex  gap-[5.25rem]'>
-                <div className='space-y-5 w-[36.25rem]'>
-                    <h2 className="text-[3rem] leading-[3.6rem] font-[500] ">The Opportunity</h2>
+            <div className='md:py-[7rem] py-[3rem] md:flex md:gap-[5.25rem] gap-[3rem]'>
+                <div className='space-y-5 md:w-[36.25rem]'>
+                    <h2 className="md:text-[3rem] text-[2.25rem] md:leading-[3.6rem] leading-[2.7rem] font-[500] ">The Opportunity</h2>
                     <div className='space-y-4'>
                         <p className='text-[1rem] leading-[1.5rem] font-[200] '>Morbi sed imperdiet in ipsum, adipiscing elit dui lectus. Tellus id scelerisque est ultricies ultricies. Duis est sit sed leo nisl, blandit elit sagittis. Quisque tristique consequat quam sed. Nisl at scelerisque amet nulla purus habitasse.</p>
                         <p className='text-[1rem] leading-[1.5rem] font-[200] '>Morbi sed imperdiet in ipsum, adipiscing elit dui lectus. Tellus id scelerisque est ultricies ultricies. Duis est sit sed leo nisl, blandit elit sagittis. Quisque tristique consequat quam sed. Nisl at scelerisque amet nulla purus habitasse.</p>
@@ -44,16 +53,16 @@ const page = () => {
                     </div>
 
                 </div>
-                <div className='w-[36.25rem] h-[33.0625rem] linerred'>
+                <div className='md:w-[36.25rem] h-[33.0625rem] linerred'>
 
                 </div>
             </div>
-            <div className='py-[7rem] flex  gap-[5.25rem]'>
-                <div className='w-[36.25rem] h-[33.0625rem] linerred'>
+            <div className='md:py-[7rem] py-[3rem] md:flex md:gap-[5.25rem] gap-[3rem]'>
+                <div className='md:w-[36.25rem] h-[33.0625rem] linerred'>
 
                 </div>
-                <div className='space-y-5 w-[36.25rem]'>
-                    <h2 className="text-[3rem] leading-[3.6rem] font-[500] ">What I did</h2>
+                <div className='space-y-5 md:w-[36.25rem]'>
+                    <h2 className="md:text-[3rem] text-[2.25rem] md:leading-[3.6rem] leading-[2.7rem] font-[500]  ">What I did</h2>
                     <div className='space-y-4'>
                         <p className='text-[1rem] leading-[1.5rem] font-[200] '>Morbi sed imperdiet in ipsum, adipiscing elit dui lectus. Tellus id scelerisque est ultricies ultricies. Duis est sit sed leo nisl, blandit elit sagittis. Quisque tristique consequat quam sed. Nisl at scelerisque amet nulla purus habitasse.</p>
                         <p className='text-[1rem] leading-[1.5rem] font-[200] '>Morbi sed imperdiet in ipsum, adipiscing elit dui lectus. Tellus id scelerisque est ultricies ultricies. Duis est sit sed leo nisl, blandit elit sagittis. Quisque tristique consequat quam sed. Nisl at scelerisque amet nulla purus habitasse.</p>
@@ -64,9 +73,9 @@ const page = () => {
                 </div>
 
             </div>
-            <div className='py-[7rem] flex gap-[5.25rem]'>
-                <div className='space-y-5 w-[36.25rem]'>
-                    <h2 className="text-[3rem] leading-[3.6rem] font-[500] ">The Outcome</h2>
+            <div className='md:py-[7rem] py-[3rem] md:flex md:gap-[5.25rem] gap-[3rem]'>
+                <div className='space-y-5 md:w-[36.25rem]'>
+                    <h2 className="md:text-[3rem] text-[2.25rem] md:leading-[3.6rem] leading-[2.7rem] font-[500]  ">The Outcome</h2>
                     <div className='space-y-4'>
                         <p className='text-[1rem] leading-[1.5rem] font-[200] '>Morbi sed imperdiet in ipsum, adipiscing elit dui lectus. Tellus id scelerisque est ultricies ultricies. Duis est sit sed leo nisl, blandit elit sagittis. Quisque tristique consequat quam sed. Nisl at scelerisque amet nulla purus habitasse.</p>
                         <p className='text-[1rem] leading-[1.5rem] font-[200] '>Morbi sed imperdiet in ipsum, adipiscing elit dui lectus. Tellus id scelerisque est ultricies ultricies. Duis est sit sed leo nisl, blandit elit sagittis. Quisque tristique consequat quam sed. Nisl at scelerisque amet nulla purus habitasse.</p>
@@ -74,19 +83,19 @@ const page = () => {
                     </div>
 
                 </div>
-                <div className='w-[36.25rem] h-[33.0625rem] linerred'>
+                <div className='md:w-[36.25rem] h-[33.0625rem] linerred'>
 
                 </div>
             </div>
             <div className='flex flex-col gap-4 py-[8.25rem]' >
-                <div className='md:w-full md:h-[40rem] linerred'>
+                <div className='md:w-full md:h-[40rem] h-[11rem] linerred'>
 
                 </div>
-                <div className='flex gap-4'>
-                    <div className='md:w-[40rem] md:h-[40rem] linerred'>
+                <div className='md:flex space-y-8 md:space-y-0 gap-4'>
+                    <div className='md:w-[40rem] md:h-[40rem] h-[20rem] linerred'>
 
                     </div>
-                    <div className='md:w-[40rem] md:h-[40rem] linerred'>
+                    <div className='md:w-[40rem] md:h-[40rem] h-[20rem] linerred'>
 
                     </div>
                 </div>
