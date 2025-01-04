bc8ae5b644ee4b9d86f715e41c5afb75ae8e6067
Christopher <odeniyichristopher7@gmail.com>
2025-01-04 01:16:26 +0000

    icons change

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import { faBars, faClose, faMoon, faSun } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -10,11 +9,11 @@ import { useEffect, useState } from "react";
 const Navigation = () => {
 
     const [isScrolled, setIsScrolled] = useState(false);
-    const [navIcon, setNavIcon] = useState(faBars);
+    const [navIcon, setNavIcon] = useState("/img/menu.png");
     const [navView, setNavView] = useState("top-[-50rem]");
 
     const navToggle = () => {
-        const iconChange = navIcon === faBars ? faClose : faBars;
+        const iconChange = navIcon === "/img/x.png" ? "/img/menu.png" : "/img/x.png";
         const nav = navView === "top-[-50rem]" ? "top-[5rem]" : "top-[-50rem]";
         setNavIcon(iconChange);
         setNavView(nav)
@@ -22,7 +21,7 @@ const Navigation = () => {
 
     const closeNavToggle = () => {
         setNavView("top-[-50rem]");
-        setNavIcon(faBars);
+        setNavIcon("/img/menu.png");
     }
 
     useEffect(() => {
@@ -76,15 +75,15 @@ const Navigation = () => {
             </div> */}
             <div className="hidden md:block">
                 {theme === "dark" ? <div>
-                    <button className=" p-2 rounded-full bg-[#FFF] " onClick={toggleTheme}><FontAwesomeIcon icon={faSun} className="w-[1.5rem] h-[1.5rem]  " /></button>
-                </div> : <div><button className=" p-2 rounded-full bg-[#FFF] border border-[#DAC5A7]" onClick={toggleTheme}> <FontAwesomeIcon icon={faMoon} className="w-[1.5rem] h-[1.5rem] " /></button></div>}
+                    <button className=" p-2 rounded-full bg-[#FFF] " onClick={toggleTheme}><Image src="/img/eclipse.gif" width={40} height={20} alt="lightmode-sun" /></button>
+                </div> : <div><button className=" p-2 rounded-full bg-[#FFF]" onClick={toggleTheme}> <Image src="/img/night-mode.gif" width={40} height={20} alt="darkmode-moon" /></button></div>}
             </div>
             <div className="bg-[#0E0E0E] h-full flex items-center px-[1rem] gap-[3.37rem] w-full justify-between md:w-fit">
-            <div className="  md:hidden">
-                {theme === "dark" ? <div>
-                    <button className=" p-2 rounded-full bg-[#FFF] " onClick={toggleTheme}><FontAwesomeIcon icon={faSun} className="w-[1.5rem] h-[1.5rem]  " /></button>
-                </div> : <div><button className=" p-2 rounded-full bg-[#FFF] border border-[#DAC5A7]" onClick={toggleTheme}> <FontAwesomeIcon icon={faMoon} className="w-[1.5rem] h-[1.5rem] " /></button></div>}
-            </div>
+                <div className="  md:hidden">
+                    {theme === "dark" ? <div>
+                        <button className=" p-2 rounded-full bg-[#FFF] " onClick={toggleTheme}><Image src="/img/eclipse.gif" width={40} height={20} alt="lightmode-sun" /></button>
+                    </div> : <div><button className=" p-2 rounded-full bg-[#FFF]" onClick={toggleTheme}> <Image src="/img/night-mode.gif" width={40} height={20} alt="darkmode-moon" /></button></div>}
+                </div>
                 <Link href="/" >
                     <div className="w-[4rem] h-[3.125rem] flex items-center justify-center">
                         <Image src="/img/KO..png" width={400} height={300} alt="KO" className="object-contain" />
@@ -92,9 +91,10 @@ const Navigation = () => {
                 </Link>
                 <div className=" ">
                     <div className="flex items-center justify-center hover:cursor-pointer md:hidden">
-                        <FontAwesomeIcon onClick={navToggle} icon={navIcon} className="w-[2.5rem] h-[2.5rem]"/>
+                        {/* <FontAwesomeIcon onClick={navToggle} icon={navIcon} className="w-[2.5rem] h-[2.5rem]" /> */}
+                        <button onClick={navToggle}> <Image src={`${navIcon}`} width={40} height={20} alt="nav icon" /></button>
                     </div>
-                    <div className={`absolute md:static min-h-[30vh] md:min-h-fit w-full md:w-auto left-0 px-2 flex duration-500 text-center items-center  ${navView} ${theme === "dark" ? "bg-[#0E0E0E] " :"bg-[#DAC5A7] md:bg-[#000] md:text-[#DAC5A7] text-[#000]" } `} >
+                    <div className={`absolute md:static min-h-[30vh] md:min-h-fit w-full md:w-auto left-0 px-2 flex duration-500 text-center items-center  ${navView} ${theme === "dark" ? "bg-[#0E0E0E] " : "bg-[#DAC5A7] md:bg-[#000] md:text-[#DAC5A7] text-[#000]"} `} >
                         <ul className="list-none  text-[1rem] leading-[1.5rem] font-[400] flex md:flex-row flex-col gap-[1.75rem]  w-full">
                             <li className=" px-[0.75rem] py-[0.5rem]"><Link onClick={closeNavToggle} href="#work">Work</Link></li>
                             <li className=" px-[0.75rem] py-[0.5rem]"><Link onClick={closeNavToggle} href="#services">Services</Link></li>
